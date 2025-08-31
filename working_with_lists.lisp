91a0c475f6b770afb2ad98cebef8e6b14c8adf72
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2025-08-31 12:56:21 +0200

    Add comments with problem number between each solution

--- working_with_lists.lisp
+++ working_with_lists.lisp
@@ -1,32 +1,50 @@
+;;;; Working with lists: P01-P28
+
+;;; P01
+
 (defun my-last (ls)
   (do ((l ls (cdr l)))
       ((not (cdr l)) l)))
+
+;;; P02
 
 (defun my-but-last (ls)
   (do ((l ls (cdr l)))
       ((not (cddr l)) l)))
 
+;;; P03
+
 (defun element-at (ls n)
   (dotimes (i (1- n)) (setf ls (cdr ls)))
   (car ls))
+
+;;; P04
 
 (defun my-length (ls)
   (do ((ls1 ls (cdr ls1))
        (counter 0 (1+ counter)))
       ((not ls1) counter)))
 
+;;; P05
+
 (defun my-reverse (ls)
   (let ((ls1 '()))
     (dolist (l ls) (push l ls1))
     ls1))
 
+;;; P06
+
 (defun palindromep (ls)
   (equal ls (reverse ls)))
+
+;;; P07
 
 (defun my-flatten (ls)
   (cond ((not ls) '())
 	((listp (car ls)) (append (flatten (car ls)) (flatten (cdr ls))))
 	(t (cons (car ls) (flatten (cdr ls))))))
+
+;;; P08
 
 (defun compress (ls)
   (do ((l ls (cdr l))
@@ -36,6 +54,8 @@
 		      (t out))))
       ((not l) (reverse out))))
 
+;;; P09
+
 (defun pack (ls)
   (let ((result '()))
     (dolist (elem ls)
@@ -44,8 +64,12 @@
 	  (push (list elem) result)))
     (reverse result)))
 
+;;; P10
+
 (defun encode (ls)
   (mapcar (lambda (group) (list (length group) (car group))) (pack ls)))
+
+;;; P11
 
 (defun encode-modified (ls)
   (mapcar (lambda (group)
@@ -53,6 +77,8 @@
 		(second group)
 		group))
 	  (encode ls)))
+
+;;; P12
 
 (defun decode (ls)
   (let ((out '()))
@@ -71,6 +97,8 @@
   (if (listp ls)
       (second ls)
       ls))
+
+;;; P13
 
 (defun encode-direct (ls)
   (let ((out '()))
@@ -91,6 +119,8 @@
       (incf (first (first ls)))
       (setf (first ls) (list 2 (first ls)))))
 
+;;; P14
+
 (defun dupli (ls)
   (let ((out '()))
     (dolist (elem ls)
@@ -98,12 +128,16 @@
       (push elem out))
     (nreverse out)))
 
+;;; P15
+
 (defun repli (ls n)
   (let ((out '()))
     (dolist (elem ls)
       (dotimes (i n)
 	(push elem out)))
     (nreverse out)))
+
+;;; P16
 
 (defun drop (ls n)
   (let ((out '())
@@ -113,6 +147,8 @@
 	(push elem out))
       (setf k (1+ k)))
     (nreverse out)))
+
+;;; P17
 
 (defun split (ls n)
   (let ((left '())
@@ -124,6 +160,8 @@
 	  (push elem right)))
     (list (nreverse left) (nreverse right))))
 
+;;; P18
+
 (defun slice (ls left right)
   (let ((out '())
 	(k 1))
@@ -132,6 +170,8 @@
 	    ((<= left k right) (push elem out)))
       (incf k))
     (nreverse out)))
+
+;;; P19
 
 (defun rotate (ls n)
   (if (null ls)
