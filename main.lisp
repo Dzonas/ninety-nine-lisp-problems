f77d47a10ca8204f72eb18bc993d9ff2a11a9c22
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2025-09-07 21:01:56 +0200

    Change top-level comment to title of problem set

--- main.lisp
+++ main.lisp
@@ -1,4 +1,5 @@
-;;;; Working with lists: P01-P28
+;;;; L-99: Ninety-Nine Lisp Problems
+;;;; https://www.ic.unicamp.br/~meidanis/courses/mc336/problemas-lisp/L-99_Ninety-Nine_Lisp_Problems.html
 
 ;;; P01
 
